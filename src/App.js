67177a0c5678b12299cc25d148e3fe4d0e6bd26d
TtinTt <128462542+TtinTt@users.noreply.github.com>
2023-05-31 20:21:36 +0700

    ví dụ đổi state

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = { studentName: "Trung" };
+
+    this.changeStudentName = this.changeStudentName.bind(this);
+  }
+
+  changeStudentName() {
+    this.setState({ studentName: "Changed" });
+  }
+
+  render() {
+    return (
+      <div>
+        Tên học viên: {this.state.studentName}{" "}
+        <button onClick={this.changeStudentName}>Đổi tên</button>
+      </div>
+    );
+  }
 }
-
 export default App;
