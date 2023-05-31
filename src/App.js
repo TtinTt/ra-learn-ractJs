edf598cdfe748fd49e1bc4922545b7090e4336a7
TtinTt <128462542+TtinTt@users.noreply.github.com>
2023-05-31 19:22:15 +0700

    session01 - bai tap 2

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  sum(num1, num2) {
+    return num1 + num2;
+  }
+  render() {
+    const num1 = 7;
+    const num2 = 4;
+
+    return (
+      <div>
+        Tổng của {num1} và {num2} là {this.sum(num1, num2)}
+      </div>
+    );
+  }
 }
 
 export default App;
