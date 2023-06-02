5ac12f3c3cf0333f8f388812561040693c0873a7
TtinTt <128462542+TtinTt@users.noreply.github.com>
2023-06-02 17:35:33 +0700

    no message

--- src/App.js
+++ src/App.js
@@ -1,46 +1,30 @@
-import logo from "./logo.svg";
+// import logo from "./logo.svg";
 import "./App.css";
 import React from "react";
 
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
-
 class App extends React.Component {
-  constructor() {
-    super();
-    this.state = { number: 0 };
+  constructor(props) {
+    super(props);
+    this.state = { number: 0, array: [], total: 0 };
   }
 
-  inceaseNumber() {
-    this.setState({
-      number: this.state.number + 1,
-    });
-  }
+  handleChangeInput = (event) => {
+    this.setState({ number: event.target.value });
+  };
+
+  handlesubbmitForm = (event) => {
+    event.preventDefault();
+    this.setState;
+  };
 
   render() {
     return (
-      <div>
-        <p>{this.state.number}</p>
-        <button onClick={() => this.inceaseNumber()}>Tăng lên 1</button>
-      </div>
+      <form onSubmit={this.handlesubbmitForm}>
+        <input type="text" onChange={this.handleChangeInput} />
+        <button type="submit">Add</button>
+        <p>Kết quả:</p>
+        <p></p>
+      </form>
     );
   }
 }
