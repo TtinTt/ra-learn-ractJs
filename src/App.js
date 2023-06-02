32f7a846c35e2d7ba2fabbded51ca75aac7002a9
TtinTt <128462542+TtinTt@users.noreply.github.com>
2023-06-02 14:57:17 +0700

    no message

--- src/App.js
+++ src/App.js
@@ -1,47 +1,13 @@
-import logo from "./logo.svg";
+import Button from "./Button";
 import "./App.css";
-import React from "react";
+import React, { Component } from "react";
 
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
-
-class App extends React.Component {
-  constructor() {
-    super();
-    this.state = { number: 0 };
-  }
-
-  inceaseNumber() {
-    this.setState({
-      number: this.state.number + 1,
-    });
-  }
-
+export default class App extends Component {
   render() {
     return (
       <div>
-        <p>{this.state.number}</p>
-        <button onClick={() => this.inceaseNumber()}>Tăng lên 1</button>
+        <Button text="Click Me" color="red" background="blue" />
       </div>
     );
   }
 }
-export default App;
---
+++ src/Button.js
@@ -0,0 +1,10 @@
+import logo from "./logo.svg";
+import "./App.css";
+import React, { Component } from "react";
+
+class Button extends React.Component {
+  render() {
+    return <button></button>;
+  }
+}
+export default Button;
