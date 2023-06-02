2e1d6071ad172b355132b5f122461e659536e31f
TtinTt <128462542+TtinTt@users.noreply.github.com>
2023-06-02 15:28:11 +0700

    no message

--- src/App.js
+++ src/App.js
@@ -2,44 +2,28 @@ import logo from "./logo.svg";
 import "./App.css";
 import React from "react";
 
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
-
 class App extends React.Component {
   constructor() {
     super();
-    this.state = { number: 0 };
+    this.state = { textInput: "" };
   }
 
-  inceaseNumber() {
+  handleGetInputValue() {
     this.setState({
       number: this.state.number + 1,
     });
   }
-
+  handleAlert() {
+    this.setState({
+      number: this.state.number + 1,
+    });
+  }
   render() {
     return (
       <div>
-        <p>{this.state.number}</p>
-        <button onClick={() => this.inceaseNumber()}>Tăng lên 1</button>
+        <input></input>
+        {/* <p>{this.state.number}</p> */}
+        <button onClick={() => this.handleAlert()}>Submit</button>
       </div>
     );
   }
