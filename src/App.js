86454b24d65c299bfc6c87580ebb1c5b33c0ff98
TtinTt <128462542+TtinTt@users.noreply.github.com>
2023-06-02 14:29:35 +0700

    no message

--- src/App.js
+++ src/App.js
@@ -23,17 +23,17 @@ import React from "react";
 //   );
 // }
 
-// class App extends React.Component {
-//   constructor() {
-//     super();
-//     this.state = { number: 0 };
-//   }
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = { number: 0 };
+  }
 
-//   inceaseNumber() {
-//     this.setState({
-//       number: this.state.number + 1,
-//     });
-//   }
+  inceaseNumber() {
+    this.setState({
+      number: this.state.number + 1,
+    });
+  }
 
   render() {
     return (
