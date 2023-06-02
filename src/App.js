ecc3c045b46f4a5f780882f1f84f4bbe962860d5
TtinTt <128462542+TtinTt@users.noreply.github.com>
2023-06-02 11:30:21 +0700

    no message

--- src/App.js
+++ src/App.js
@@ -1,45 +1,18 @@
-import logo from "./logo.svg";
+// import logo from "./logo.svg";
 import "./App.css";
 import React from "react";
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
+import Header from "./component/Header";
+import Item from "./component/Item";
+import ListItem from "./component/ListItem";
 
 class App extends React.Component {
-  constructor() {
-    super();
-    this.state = { number: 0 };
-  }
-
-  inceaseNumber() {
-    this.setState({
-      number: this.state.number + 1,
-    });
-  }
-
   render() {
     return (
       <div>
-        <p>{this.state.number}</p>
-        <button onClick={() => this.inceaseNumber()}>Tăng lên 1</button>
+        <Header />
+        <ListItem />
+
+        <Item />
       </div>
     );
   }
