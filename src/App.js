3a7c415496d90a5dfa8b5348df46c2c989043d41
TtinTt <128462542+TtinTt@users.noreply.github.com>
2023-06-05 21:11:57 +0700

    no message

--- src/App.js
+++ src/App.js
@@ -1,47 +1,29 @@
-import logo from "./logo.svg";
+import { useState, useReducer } from "react";
 import "./App.css";
-import React from "react";
 
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
+function App() {
+  // const [count, setCount] = useState(0)
+  const UP = "UP";
+  const DOWN = "DOWN";
 
-class App extends React.Component {
-  constructor() {
-    super();
-    this.state = { number: 0 };
-  }
+  const reducer = (state, action) => {
+    if (action === UP) {
+      return state + 1;
+    } else if (action === DOWN) {
+      return state - 1;
+    }
+  };
 
-  inceaseNumber() {
-    this.setState({
-      number: this.state.number + 1,
-    });
-  }
+  const [count, dispatch] = useReducer(reducer, 0);
 
-  render() {
-    return (
-      <div>
-        <p>{this.state.number}</p>
-        <button onClick={() => this.inceaseNumber()}>Tăng lên 1</button>
-      </div>
-    );
-  }
+  return (
+    <div className="App">
+      <h1>REDUCER</h1>
+      <p>Count: {count}</p>
+      <button onClick={() => dispatch(UP)}>Up</button>
+      <button onClick={() => dispatch(DOWN)}>Down</button>
+    </div>
+  );
 }
+
 export default App;
