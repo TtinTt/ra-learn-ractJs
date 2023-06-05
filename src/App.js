82f613db774e0177f91abdd1955ff194c363a36c
TtinTt <128462542+TtinTt@users.noreply.github.com>
2023-06-05 21:25:04 +0700

    no message

--- src/App.js
+++ src/App.js
@@ -1,47 +1,17 @@
-import logo from "./logo.svg";
-import "./App.css";
-import React from "react";
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
-
-class App extends React.Component {
-  constructor() {
-    super();
-    this.state = { number: 0 };
-  }
-
-  inceaseNumber() {
-    this.setState({
-      number: this.state.number + 1,
-    });
-  }
-
-  render() {
-    return (
-      <div>
-        <p>{this.state.number}</p>
-        <button onClick={() => this.inceaseNumber()}>Tăng lên 1</button>
-      </div>
-    );
-  }
+import React, { useState } from "react";
+import Counter from "./React/Counter";
+import OrderForm from "./React/OrderForm";
+import ProductList from "./React/ProductList";
+function App() {
+  return (
+    <div>
+      <Counter />
+      <hr />
+      <OrderForm />
+      <hr />
+      <ProductList />
+    </div>
+  );
 }
+
 export default App;
