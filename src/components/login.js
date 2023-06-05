cb53b11ba0ce2453bf92b7135c9eec0dabac463f
TtinTt <128462542+TtinTt@users.noreply.github.com>
2023-06-05 19:50:49 +0700

    no message

--- src/components/login.js
+++ src/components/login.js
@@ -3,7 +3,7 @@ import React from "react";
 class Login extends React.Component {
   constructor() {
     super();
-    this.state = { username: "", password: "" };
+    this.state = { username: "", password: "", errors: {} };
   }
 
   handleChange = async (event) => {
@@ -15,7 +15,44 @@ class Login extends React.Component {
     console.log(this.state);
   };
 
-  handleSubmit = (event) => {};
+  handleSubmit = async (event) => {
+    await this.validateUsername();
+    await this.validatePassword();
+
+    event.preventDefault();
+  };
+
+  validateUsername = async () => {
+    const errors = this.state.errors;
+    const username = this.state.username;
+    if (username.length === 0) {
+      errors.username = "Tên đăng nhập là bắt buộc";
+    } else if (username.length < 4 || username.length > 10) {
+      errors.username = "Tên đăng nhập cần dài từ 4 đến 10 ký tự";
+    }
+    //sử dụng regular expression (regex)
+    else if (!username.match(/^[A-Za-z0-9]+$/)) {
+      errors.username = "Tên đăng nhập chỉ được bao gồm chữ và số";
+    }
+    console.log(errors.username);
+    await this.setState({ errors: { ...this.state.errors, ...errors } });
+  };
+
+  validatePassword = async () => {
+    const errors = this.state.errors;
+    const password = this.state.password;
+    if (password.length === 0) {
+      errors.password = "Tên đăng nhập là bắt buộc";
+    } else if (password.length < 4 || password.length > 10) {
+      errors.password = "Tên đăng nhập cần dài từ 4 đến 10 ký tự";
+    }
+    //sử dụng regular expression (regex)
+    else if (!password.match(/^[A-Za-z0-9]+$/)) {
+      errors.password = "Tên đăng nhập chỉ được bao gồm chữ và số";
+    }
+    console.log(errors.password);
+    await this.setState({ errors: { ...this.state.errors, ...errors } });
+  };
 
   render() {
     return (
@@ -30,6 +67,9 @@ class Login extends React.Component {
               name="username"
               onChange={this.handleChange}
             ></input>
+            <p style={{ color: "red" }}>
+              {this.state.errors.username ? this.state.errors.username : null}
+            </p>
           </div>
           <div>
             <label>Mật khẩu</label>
@@ -39,9 +79,18 @@ class Login extends React.Component {
               name="password"
               onChange={this.handleChange}
             ></input>
+            <p style={{ color: "red" }}>
+              {this.state.errors.password ? this.state.errors.password : null}
+            </p>
           </div>
           <div>
             <button>Đăng nhập</button>
+          </div>
+          <div>
+            <button type="reset">Reset</button>
+          </div>
+          <div>
+            <button type="button">Đăng nhập bằng Facebook</button>
           </div>
         </div>
       </form>
