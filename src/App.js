f1576196f7323dc75371dc64e162d75b6fa29240
TtinTt <128462542+TtinTt@users.noreply.github.com>
2023-06-07 19:39:43 +0700

    no message

--- src/App.js
+++ src/App.js
@@ -1,47 +1,17 @@
-import logo from "./logo.svg";
-import "./App.css";
 import React from "react";
+import { DatePicker, Space } from "antd";
 
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
+const { RangePicker } = DatePicker;
 
-class App extends React.Component {
-  constructor() {
-    super();
-    this.state = { number: 0 };
-  }
+const App: React.FC = () => (
+  <Space direction="vertical" size={12}>
+    <RangePicker />
+    <RangePicker showTime />
+    <RangePicker picker="week" />
+    <RangePicker picker="month" />
+    <RangePicker picker="quarter" />
+    <RangePicker picker="year" />
+  </Space>
+);
 
-  inceaseNumber() {
-    this.setState({
-      number: this.state.number + 1,
-    });
-  }
-
-  render() {
-    return (
-      <div>
-        <p>{this.state.number}</p>
-        <button onClick={() => this.inceaseNumber()}>Tăng lên 1</button>
-      </div>
-    );
-  }
-}
 export default App;
