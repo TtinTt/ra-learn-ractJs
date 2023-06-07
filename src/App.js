fbc122fd57df7870c6282ca117457cfcd1e1b030
TtinTt <128462542+TtinTt@users.noreply.github.com>
2023-06-07 20:22:14 +0700

    no message

--- src/App.js
+++ src/App.js
@@ -3,20 +3,11 @@ import "./App.css";
 import React from "react";
 
 class App extends React.Component {
-  // constructor() {
-  //   super();
-  //   this.state = { number: 0 };
-  // }
-
-  // inceaseNumber() {
-  //   this.setState({
-  //     number: this.state.number + 1,
-  //   });
-  // }
-
   render() {
     return (
       <div>
+        <h1>MINI PROJECT TODO LIST</h1>
+        <input placeholder="Thêm công việc"></input>
         <input type="checkbox"></input>
       </div>
     );
