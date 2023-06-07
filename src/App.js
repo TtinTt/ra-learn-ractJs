2b47ccbc56b3b2f8b70624f77c8cf5bacb1cca90
TtinTt <128462542+TtinTt@users.noreply.github.com>
2023-06-07 19:28:17 +0700

    ss05

--- src/App.js
+++ src/App.js
@@ -1,45 +1,14 @@
-import logo from "./logo.svg";
-import "./App.css";
 import React from "react";
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
+import Button from "react-bootstrap/Button";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 class App extends React.Component {
-  constructor() {
-    super();
-    this.state = { number: 0 };
-  }
-
-  inceaseNumber() {
-    this.setState({
-      number: this.state.number + 1,
-    });
-  }
-
   render() {
     return (
       <div>
-        <p>{this.state.number}</p>
-        <button onClick={() => this.inceaseNumber()}>Tăng lên 1</button>
+        <Button variant="danger">button react bootstrap</Button>
+        <hr />
+        <button>button html</button>
       </div>
     );
   }
