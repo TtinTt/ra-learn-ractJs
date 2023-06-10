7471e40be9f617a54efe6b8a8d805549e4211da3
TtinTt <128462542+TtinTt@users.noreply.github.com>
2023-06-10 10:00:53 +0700

    no message

--- src/App.js
+++ src/App.js
@@ -1,29 +1,43 @@
 import logo from "./logo.svg";
-import "./App.css";
 import React from "react";
 
 class App extends React.Component {
   constructor() {
     super();
-    this.state = {
-      currentTime: "",
-    };
+    this.state = { time: "" };
+  }
+
+  handleTime = () => {
+    let thisTime = new Date();
+    let hour = thisTime.getHours();
+    if (hour < 10) {
+      hour = "0" + hour;
+    }
+    let minite = thisTime.getMinutes();
+    if (minite < 10) {
+      minite = "0" + minite;
+    }
+    let sec = thisTime.getSeconds();
+    if (sec < 10) {
+      sec = "0" + sec;
+    }
+    this.setState({ time: "Bây giờ là " + hour + ":" + minite + ":" + sec });
+  };
+
+  componentWillMount() {
+    setInterval(this.handleTime, 1000);
   }
 
   render() {
-    let datetime = new Date();
-
-    setInterval(function () {
-      let datetime = new Date();
-    }, 1000);
-
     return (
-      <div>
-        <h1>Đồng hồ </h1>
-        <h2> {datetime.toLocaleTimeString()}</h2>
+      <div className="App">
+        <h1>Xin chào các bạn!</h1>
+        <h2>{this.state.time}</h2>
       </div>
     );
   }
+
+  // componentWillUnmount() {}
 }
 
 export default App;
