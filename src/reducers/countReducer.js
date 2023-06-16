e1c00bb5483d00f045668f95ee8d4fbcdce9d750
TtinTt <128462542+TtinTt@users.noreply.github.com>
2023-06-16 21:12:17 +0700

    no message

---
+++ src/reducers/countReducer.js
@@ -0,0 +1,26 @@
+const initState = {
+  count: 3,
+  // todos: []
+};
+
+const countReducer = (state = initState, action) => {
+  switch (action.type) {
+    case "UP_COUNT":
+      console.log("countReducer", "UP_COUNT");
+      return {
+        // ...state
+        count: state.count + action.payload,
+        // todo: action.payload // {name: 'task 1', isFinished: false}
+      };
+    case "DOWN_COUNT":
+      console.log("countReducer", "DOWN_COUNT");
+      return {
+        // ...state
+        count: state.count - action.payload,
+      };
+    default:
+      return state;
+  }
+};
+
+export default countReducer;
